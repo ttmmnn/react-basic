a5ac240ba140190a93185735d5c13ff690672b48
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-07-16 23:44:01 +0900

    update

--- src/components/BadArticle.jsx
+++ src/components/BadArticle.jsx
@@ -1,10 +1,10 @@
-import { Content, Title } from './index';
+import { Content, Title } from '.';
 
 const BadArticle = (props) => {
   return (
     <div>
       <div className="flex__row">
-        <img src={'/logo192.png'} with={36} height={36} />
+        <img src={'/logo512.png'} with={36} height={36} />
         <h2>{props.title}</h2>
       </div>
       <Content content={props.content} />
